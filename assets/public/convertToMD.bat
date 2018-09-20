f43b99561d7624403a17a1d45164be2bab6d84e3
Prashant <prashant.bucha@comprotechnologies.com>
2018-09-20 15:01:22 +0530

    Updating convertToMD.bat file to update 1.1 content

--- assets/public/convertToMD.bat
+++ assets/public/convertToMD.bat
@@ -1,4 +1,6 @@
 rem ::Chapter_6_Building_A_Model
+pandoc -S "./Chapter_1_Introduction/documents/01_The_Company.docx" -M docPath="./Chapter_1_Introduction/documents/01_The_Company.docx" -t json | node pandoc_filter.js  | pandoc -f json -t commonmark --atx-headers --wrap=preserve -o "./Chapter_1_Introduction/01_The_Company.md" & node update_md.js "./Chapter_1_Introduction/01_The_Company.md"
+
 pandoc -S "./Chapter_6_Building_A_Model/documents/01_Should_LeNapoleon_Open_A_New_Bakery.docx" -M docPath="./Chapter_6_Building_A_Model/documents/01_Should_LeNapoleon_Open_A_New_Bakery.docx" -t json | node pandoc_filter.js  | pandoc -f json -t commonmark --atx-headers --wrap=preserve -o "./Chapter_6_Building_A_Model/01_Should_LeNapoleon_Open_A_New_Bakery.md" & node update_md.js "./Chapter_6_Building_A_Model/01_Should_LeNapoleon_Open_A_New_Bakery.md"
 
 pandoc -S "./Chapter_6_Building_A_Model/documents/02_Dummy.docx" -M docPath="./Chapter_6_Building_A_Model/documents/02_Dummy.docx" -t json | node pandoc_filter.js  | pandoc -f json -t commonmark --atx-headers --wrap=preserve -o "./Chapter_6_Building_A_Model/02_Dummy.md" & node update_md.js "./Chapter_6_Building_A_Model/02_Dummy.md"
